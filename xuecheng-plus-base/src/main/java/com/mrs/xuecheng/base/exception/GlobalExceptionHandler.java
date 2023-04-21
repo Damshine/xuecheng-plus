bb6896e23730eb410c3c4a33c6918ea9873eb2e8
Damshine <2314028487@qq.com>
2023-04-21 09:19:40 +0800

    引入jsr303校验

--- xuecheng-plus-base/src/main/java/com/mrs/xuecheng/base/exception/GlobalExceptionHandler.java
+++ xuecheng-plus-base/src/main/java/com/mrs/xuecheng/base/exception/GlobalExceptionHandler.java
@@ -2,9 +2,14 @@ package com.mrs.xuecheng.base.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 /**
  * description: GlobalExceptionHandler
@@ -30,5 +35,20 @@ public class GlobalExceptionHandler {
         log.error("【系统异常】{}", e.getMessage(), e);
         return new RestErrorResponse(CommonError.UNKOWN_ERROR.getErrorMessage());
     }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestErrorResponse doValidException(MethodArgumentNotValidException argumentNotValidException) {
+        BindingResult bindingResult = argumentNotValidException.getBindingResult();
+
+        StringBuffer errMsg = new StringBuffer();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        fieldErrors.forEach(error->{
+            errMsg.append(error.getDefaultMessage()).append(",");
+        });
+        log.error(errMsg.toString());
+        return new RestErrorResponse(errMsg.toString());
+    }
+
 
 }
