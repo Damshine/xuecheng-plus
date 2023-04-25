e956b4465bb12b7e0dce498c36e4a50d0883632d
Damshine <2314028487@qq.com>
2023-04-25 18:02:26 +0800

    引入微服务网关

---
+++ xuecheng-plus-gateway/src/test/java/com/hxut/mrs/xuechengplus/gateway/XuechengPlusGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxut.mrs.xuechengplus.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
